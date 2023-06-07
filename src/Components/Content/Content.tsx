695c5904a57782f77d530bdbcf15026e129d57ee
alimo <alimoallemi90@gmail.com>
2023-06-07 18:15:17 +0330

    Content.jsx changed to Content.tsx

--- src/Components/Content/Content.tsx
+++ src/Components/Content/Content.tsx
@@ -1,21 +1,21 @@
-import { useState, useRef } from "react";
+import React, { useState, useRef} from "react";
 import Cards from "./Cards/Cards";
 import "./Content.css";
 import AddInput from "./AddInput/AddInput";
 import SearchInput from "../SearchInput/SearchInput";
+import { notes } from "../../interfaces/Interfaces";
 
-const showTime = (time) => {
+const showTime = (time : number) => {
   return time < 10 ? "0" + time : time;
 };
 
 function Content() {
-  const database = JSON.parse(localStorage.getItem("data") || []);
+  const database : notes[] = JSON.parse(localStorage.getItem("data")!) || [];
   const [notes, setNotes] = useState(database);
-
-  const [search, setSearch] = useState([]);
+  const [search, setSearch] = useState<notes[]>([]);
   const [inSearch, setInSearch] = useState(false);
-  const [currentSearchValue, setCurrentSearchValue] = useState("");
-  const searchInputRef = useRef(null);
+  const [currentSearchValue, setCurrentSearchValue] = useState<string>("");
+  const searchInputRef = useRef<HTMLInputElement>(null);
 
   const [currentAddValue, setCurrentAddValue] = useState("");
 
@@ -39,16 +39,16 @@ function Content() {
     }
   };
 
-  const deleteTextHandler = (deleteIndex) => {
+  const deleteTextHandler = (deleteIndex : number) => {
     const newNotes = [...notes];
     newNotes.splice(deleteIndex, 1);
     setInSearch(false);
-    searchInputRef.current.value = "";
+    searchInputRef.current!.value = "";
     setNotes(newNotes);
     localStorage.setItem("data", JSON.stringify(newNotes));
   };
 
-  const searchHandler = (e) => {
+  const searchHandler = (e : any) => {
     const searchValue = e.target.value;
     setCurrentSearchValue(searchValue);
     const searchedArray = notes.filter((note) => {
@@ -77,7 +77,7 @@ function Content() {
         />
         <Cards
           Notes={inSearch ? search : notes}
-          DeleteText={(index) => deleteTextHandler(index)}
+          DeleteText={(index: number) => deleteTextHandler(index)}
         />
       </div>
     </>
