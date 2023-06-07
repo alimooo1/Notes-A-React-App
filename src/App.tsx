aa15091cdac4ebf06650c07520ff3ec88b5c2182
alimo <alimoallemi90@gmail.com>
2023-06-07 18:18:54 +0330

    App.js changed to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import Content from './Components/Content/Content';
 import Header from './Components/Header/Header';
+import React from 'react';
 
 function App() {
   return (
