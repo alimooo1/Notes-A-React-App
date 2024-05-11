945fec1308d49ec3ff98b1c0efe636e450ca2085
Ali <alimoallemi30@gmail.com>
2024-05-11 14:51:32 +0330

    Get Telegram Settings

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
+// @ts-ignore
+const appData = window.Telegram.WebApp;
+console.log(appData)
 root.render(
   <React.StrictMode>
     <App />
